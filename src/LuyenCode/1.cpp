6a5e6e4c3ca923253e3624ca31ecccfc93bc60b5
Dung <72554580+dungpi@users.noreply.github.com>
2021-05-30 17:21:54 +0700

    Delete 1.cpp

--- src/LuyenCode/1.cpp
+++
@@ -1,9 +0,0 @@
-void print (Sach *&sach, int &n){
-    cout <<"Nhap So Luong Sach: ";
-    cin >> n;
-    sach = new (Sach);
-    for (int i = 0;i < n; i++){
-    cout <<"Quyen Sach "<<i+1<<endl;
-    print (sach+i);
-    }
-}
